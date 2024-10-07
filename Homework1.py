44924521cc1b6722b1867de82a2e7dbfdd8f2ffc
unknown <vodamianov@gmail.com>
2024-10-07 14:49:22 +0300

    first part of the homework

--- Homework1.py
+++ Homework1.py
@@ -1,1 +1,29 @@
-print ("hello")
+
+#Exercise one - Write a Python script that prints "Hello, World!" to the console.
+print ("Hello, World!")
+
+#Exercise two - Create variables to store your name, age, and favorite color. Then, print out a message using these variables (e.g., "My name is [name], I am [age] years old, and my
+#favorite color is [color].").
+
+name = 'Vassil'
+age = 36
+favorite_color = 'purple'
+
+print ("My name is " + name)
+print ("I am " , age, " years old, and my favorite color is " + favorite_color)
+
+#Exercise three - Write a program that calculates the area of a rectangle. Prompt the user to enter the length and width, calculate the area, and then print it.
+
+length = input("Enter length: ")
+width = input("Enter width: ")
+
+rectangle = (int(length)*int(width))
+print ("The are of the rectangle is ", int(rectangle))
+
+#Exercise four - Write a program that converts temperatures from Fahrenheit to Celsius. Prompt the user to enter a temperature in Fahrenheit and then print out the equivalent
+#temperature in Celsius.
+
+tempF = input("Enter temperature in F: ")
+Celsius = ((float(tempF) - 32) / 1.8)
+
+print ("Converted temperature in Celsius is: ",  float(Celsius))
